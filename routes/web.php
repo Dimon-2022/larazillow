6e642cc272809682306375627ecac9dc35db9057
dima.moisieienko <dimonmail09@gmail.com>
2026-02-20 20:16:20 +0200

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\IndexController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [IndexController::class, 'index']);
+
+
+Route::get('/show', [IndexController::class, 'show']);
