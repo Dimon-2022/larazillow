94c5ae861acddf98a5497815cdb290dd7b2274a3
dima.moisieienko <dimonmail09@gmail.com>
2026-03-01 21:41:23 +0200

    creating resource controller, implemented actions index and show, created vue components

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index']);
+//Route::get('/', [IndexController::class, 'index']);
+//
+//
+//Route::get('/show', [IndexController::class, 'show']);
+//
+//Route::get('/greeting/{name}', [IndexController::class, 'greeting']);
 
 
-Route::get('/show', [IndexController::class, 'show']);
+Route::resource('listing', ListingController::class)->only(['index', 'show']);
 
-Route::get('/greeting/{name}', [IndexController::class, 'greeting']);
