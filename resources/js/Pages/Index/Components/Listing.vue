74f37a975efff839f74e4e9e880d78f55f0b3daa
dima.moisieienko <dimonmail09@gmail.com>
2026-03-15 21:19:30 +0200

    calculator component for listing page

---
+++ resources/js/Pages/Index/Components/Listing.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <Box>
+        <div>
+            <Link :href="route('listing.show', listing.id)">
+                <div class="flex items-center gap-1">
+                    <Price :price="listing.price" class="text-2xl font-bold"/>
+                    <div class="text-xs text-gray-500">
+                        <Price :price="400"/> pm
+                    </div>
+                </div>
+                <ListingSpace :listing="listing" class="text-lg"/>
+                <ListingAddress :listing="listing" class="text-gray-500"/>
+            </Link>
+        </div>
+        <div>
+            <Link :href="route('listing.edit', listing.id)">Edit</Link>
+        </div>
+        <div>
+            <Link method="DELETE" :href="route('listing.destroy', listing.id)" as="button">Delete</Link>
+        </div>
+    </Box>
+</template>
+
+<script setup>
+import {Link} from "@inertiajs/vue3"
+import ListingAddress from "../../Components/ListingAddress.vue";
+import Box from "@/Components/UI/Box.vue";
+import ListingSpace from "@/Components/ListingSpace.vue";
+import Price from "@/Components/Price.vue";
+
+defineProps({
+    listing: Object
+})
+</script>
