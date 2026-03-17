64acc66810e0b18a08005683c55c7364f236896e
dima.moisieienko <dimonmail09@gmail.com>
2026-03-17 22:33:18 +0200

    implemented vue for login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
@@ -13,4 +14,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::resource('listing', ListingController::class)->only(['index', 'show', 'create', 'store', 'edit','update', 'destroy']);
+
+Route::get('/login', [AuthController::class, 'create'])->name('login');
+
+Route::post('/login', [AuthController::class, 'store'])->name('login.store');
+
+Route::delete('/logout', [AuthController::class, 'destroy'])->name('logout');
 
