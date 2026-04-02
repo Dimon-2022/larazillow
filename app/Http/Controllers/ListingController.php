4404ffc9908434c947e6743daa2a6633c16cf396
dima.moisieienko <dimonmail09@gmail.com>
2026-04-02 23:23:41 +0300

    policy restrictions

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,9 +9,10 @@ use Illuminate\Support\Facades\Auth;
 class ListingController extends Controller
 {
 
-//    public function __construct(){
+    public function __construct(){
+//        $this->authorizeResource(Listing::class, 'listing');
 //        $this->middleware('auth')->except('index', 'show');
-//    }
+    }
     /**
      * Display a listing of the resource.
      */
@@ -30,6 +31,7 @@ class ListingController extends Controller
      */
     public function create()
     {
+//        $this->authorize('create', Listing::class);
         return inertia('Listing/Create');
     }
 
@@ -60,6 +62,16 @@ class ListingController extends Controller
      */
     public function show(Listing $listing)
     {
+//        if(Auth::user()->cannot('view', $listing)) {
+//            abort(403);
+//        }
+
+//        $this->authorize('view', $listing);
+
+
+
+
+
         return inertia(
             'Listing/Show',
             [
