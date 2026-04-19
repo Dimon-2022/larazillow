329953872c5eaa1b5ee2b94402c3c86d64d59c6a
dima.moisieienko <dimonmail09@gmail.com>
2026-04-19 23:07:09 +0300

    sorting and filtering

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -13,6 +13,8 @@ class Listing extends Model
 
     protected $fillable = ['beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'];
 
+    protected $sortable = ['price', 'created_at'];
+
     public function owner(): BelongsTo {
         return $this->belongsTo(\App\Models\User::class, 'by_user_id');
     }
@@ -40,6 +42,15 @@ class Listing extends Model
         )->when(
             $filters['areaTo'] ?? false,
             fn($query, $value) => $query->where('area', '<=', $value)
+        )->when(
+            $filters['deleted'] ?? false,
+            fn($query, $value) => $query->withTrashed()
+        )->when(
+            $filters['by'] ?? false,
+            fn($query, $value) =>
+            !in_array($value, $this->sortable)
+                ? $query :
+                $query->orderBy($value, $filters['order'] ?? 'desc')
         );
     }
 }
