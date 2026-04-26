cb65207386c6f8c25d76bcd4ebbfbde8d0c5ba9c
dima.moisieienko <dimonmail09@gmail.com>
2026-04-26 16:59:07 +0300

    uploading images on backend side

---
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -0,0 +1,12 @@
+<template>
+    <form :action="route('realtor.listing.image.store', {listing: listing.id})" method="post" enctype="multipart/form-data">
+        <input type="file" multiple name="images[]"/>
+        <button type="submit">Send</button>
+    </form>
+</template>
+
+<script setup>
+    defineProps({
+        listing: Object
+    })
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\RealtorListingController;
+use App\Http\Controllers\RealtorListingImageController;
 use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
 
@@ -33,6 +34,8 @@ Route::prefix('realtor')
     ->group(function(){
         Route::name('listing.restore')->put('listing/{listing}/restore', [RealtorListingController::class, 'restore'])->withTrashed();
         Route::resource('listing', RealtorListingController::class)->only(['index'])->only(['index','destroy', 'edit', 'update', 'create', 'store'])->withTrashed();
+
+        Route::resource('listing.image', RealtorListingImageController::class)->only(['store', 'create']);
     });
 
 
