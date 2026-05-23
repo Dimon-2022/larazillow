a44af88d46a4589ccb96b27bfba1a7be6b73fa29
dima.moisieienko <dimonmail09@gmail.com>
2026-05-23 14:25:31 +0300

    resent link and finished project

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\RealtorListingImageController;
 use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 //Route::get('/', [IndexController::class, 'index'])->middleware('auth');
 //
@@ -40,6 +42,19 @@ Route::get('/email/verify', function () {
     return inertia('Auth/VerifyEmail');
 })
     ->middleware('auth')->name('verification.notice');
+
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect()->route('listing.index')->with('success', 'Email verified successfully.');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('success', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::resource('user-account', UserAccountController::class)->only(['create', 'store']);
 
